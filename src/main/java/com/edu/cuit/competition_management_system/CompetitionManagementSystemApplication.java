f650a5f6811a44afdf0347e8eae383f7b5a7988b
yuanck <627261542@qq.com>
2019-11-20 10:19:33 +0800

    环境搭建

---
+++ src/main/java/com/edu/cuit/competition_management_system/CompetitionManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.edu.cuit.competition_management_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompetitionManagementSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CompetitionManagementSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/edu/cuit/competition_management_system/CompetitionManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edu.cuit.competition_management_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompetitionManagementSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
